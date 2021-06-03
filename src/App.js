40e5de94c1078e5dc03b2b39ce21c1ff64df91e3
Aditya Pundir <68149659+Aditya-Pundir@users.noreply.github.com>
2021-06-03 00:05:58 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+// import Signup from "./components/Signup";
+import SignInScreen from "./SignInScreen";
+// import { Container } from "react-bootstrap";
+
+function App() {
+  return (
+    <>
+      {/* <Container
+        className="d-flex align-items-center justify-content-center"
+        style={{ minHeight: "100vh" }}
+      >
+        <div className="w-100" style={{ maxWidth: "400px" }}>
+          <Signup />
+        </div>
+      </Container> */}
+      <SignInScreen />
+    </>
+  );
+}
+
+export default App;
